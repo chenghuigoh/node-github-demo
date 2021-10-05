29546822e9d5fff7840a3a90628502ec55a1e334
Hana <chenghui14@gmail.com>
2021-10-05 11:30:29 +0800

    test for server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
  // Respond with "hello world" for requests that hit our root "/"
  app.get("/", function (req, res) {
-  return res.send("Hello Universe");
+  return res.send("Hello Universe, is there anyone?");
  });
 
  // listen to port 7000 by default
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-  it("respond with Hello Universe", (done) => {
-    request(app).get("/").expect("Hello Universe", done);
+  it("respond with Hello Universe, is there anyone?", (done) => {
+    request(app).get("/").expect("Hello Universe, is there anyone?", done);
   })
 });
